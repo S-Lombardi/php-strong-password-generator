9f12d3419dd5d608dfc6ac2b002433140b124b92
S-Lombardi <silo.lombardi@gmail.com>
2023-07-09 21:18:16 +0200

    aggiunto file css, modifiche di stile

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-    session_start();
-    //include ("functions.php");
-
     include __DIR__.'/functions.php';
+    // oppure --> include ("functions.php");
+
     generateRandomPsw();
 ?>
 
@@ -12,29 +11,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="./css/style.css">
     <title>Password Generate</title>
 </head>
 <body>
-    <div class="container">
-        <div class="bg-primary mt-5 p-4 rounded">
-            <div class="row justify-content-between">
-                <div class="col-auto">
-                    <form class="form" action="./index.php" method="GET">
-                        <!-- Input per scegliere il numero dei caratteri -->
-                        <label for="quantity" class="text-">Scegli un numero per generare la tua password</label>
-                        <input type="number" id="quantity" name="character_quantity" min="5" max="15" placeholder="..." class="ms-4">          <input type="submit" value="Invia">
-                    </form>
-                    
-                </div>
+    <div class="wrapper">
 
+        <div class="container">
+            <div class="bg-light mt-5 p-4 rounded ">
+                <div class="row">
+                    <div class="col-auto">
+                        <form class="form" action="./index.php" method="GET">
+                            <label for="quantity" class="text-">Scegli un numero per generare la tua password</label>
+                            <!-- CAMPO INPUT per scegliere il numero dei caratteri -->
+                            <input type="number" id="quantity" name="character_quantity" min="5" max="15" placeholder="..." class="ms-4">
+                            <!-- PULSANTE -->
+                            <input type="submit" value="Invia" class=" ">
+                        </form>
+                        
+                    </div>
+                </div>
             </div>
-            <!-- PULSANTE -->
-  
-            
         </div>
     </div>
-    
-    
 </body>
 </html>
